8148d933009154f2f640336be8b2073899b7db7b
Somto111 <130008689+Somto111@users.noreply.github.com>
2025-09-24 01:30:50 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,81 @@
+import hashlib
+import os
+from flask import Flask, request, jsonify, render_template, flash, url_for
+from dotenv import load_dotenv
+import mailchimp_marketing as MailchimpMarketing
+from mailchimp_marketing.api_client import ApiClientError
+from unicodedata import category
+from werkzeug.utils import redirect
+
+load_dotenv()
+
+app = Flask(__name__)
+app.secret_key = "mysupersecretkey123"
+
+MAILCHIMP_API_KEY = os.getenv("MAILCHIMP_API_KEY")
+MAILCHIMP_SERVER_PREFIX = os.getenv("MAILCHIMP_SERVER_PREFIX")
+MAILCHIMP_LIST_ID =  os.getenv("MAILCHIMP_LIST_ID")
+
+
+client = MailchimpMarketing.Client()
+client.set_config({
+    "api_key": MAILCHIMP_API_KEY,
+    "server": MAILCHIMP_SERVER_PREFIX
+})
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/join_waitlist', methods=['GET','POST'])
+def join_waitlist():
+    if request.method =='GET':
+        return "waitlist endpoint is available"
+
+    email = request.form.get('email')
+
+    if not email or '@' not in email:
+        flash("please enter a valid email.", "error")
+        return redirect(url_for('home'))
+        # return jsonify({
+        #     "status": "error",
+        #     "message": "Invalid_email"
+        # }),400
+    try:
+       response= client.lists.add_list_member(MAILCHIMP_LIST_ID,{
+           "email_address": email,
+           "status": "subscribed"
+       })
+
+       flash("You are on the waitlist!", "success")
+       return redirect(url_for('home'))
+       # return jsonify({
+       #     "status": "success",
+       #     "message": "You're on the waitlist"
+       # })
+    except ApiClientError as error:
+        # flash(f" Error: {error.text}", "error")
+        #
+        # return redirect(url_for('home'))
+        # return jsonify({
+        #     "status": "error",
+        #     "message": str(error)
+        # }),400
+        error_msg = str("you have been registered")
+
+        if "Member Exists" in error_msg:
+            subscriber_hash = hashlib.md5(email.lower().encode()).hexdigest()
+            client.lists.set_list_member(MAILCHIMP_LIST_ID, subscriber_hash,{
+                "email_address": email,
+                "status_if_new": "subscribed",
+                "status": "subscribed"
+            })
+            flash("You are already on the waitlist!", 'info')
+            return redirect(url_for('home'))
+
+        flash( error_msg, "error")
+        return redirect(url_for('home'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
